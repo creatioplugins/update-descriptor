2c09061a9a47e4953560482b1f69070d8838efa8
creatioplugins <68465233+creatioplugins@users.noreply.github.com>
2020-07-18 14:00:13 +0300

    First commit
    First commit

---
+++ spec/update-descriptor-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import UpdateDescriptorView from '../lib/update-descriptor-view';
+
+describe('UpdateDescriptorView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
